cfe16af4a4acaef88e99685ba4f0733786930d31
dliu12 <ldiyi997l@gmail.com>
2021-07-17 23:17:45 -0400

    started react app

---
+++ stats-preview-card-component-main/my-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+import { Card } from './components/card/card.js'
+
+function App() {
+    return (
+        <Card />
+        // <div>
+        //     Get insights that help your business grow. Discover the benefits of data analytics and
+        //     make better decisions regarding revenue, customer experience, and overall efficiency.
+        //     10k+ companies 314 templates 12m+ queries
+        //     <div>
+        //         Challenge by{' '}
+        //         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        //             Frontend Mentor
+        //         </a>
+        //         Coded by <a href="#">Your Name Here</a>.
+        //     </div>
+        // </div>
+    )
+}
+
+export default App
---
+++ stats-preview-card-component-main/my-app/src/components/card/card.js
@@ -0,0 +1,6 @@
+import './card.css'
+const Card = () => {
+    return <div>hello</div>
+}
+
+export default Card
